d5289e77f7b62bae4c7e79c95774fd1a3cbb223e
p-didier <paul.didier@esat.kuleuven.be>
2025-03-15 10:47:29 +0100

    included unprocessed in plot

--- main.py
+++ main.py
@@ -9,10 +9,11 @@
 # (c) Paul Didier, SOUNDS ETN, KU Leuven ESAT STADIUS
 
 import sys, time
-import numpy as np
 import mypalettes
+import numpy as np
 from tools.algos import *
 from tools.base import *
+from pyinstrument import Profiler
 
 import matplotlib as mpl
 mpl.use('TkAgg')  # use TkAgg backend to avoid issues when plotting
@@ -32,14 +33,20 @@ def main():
 
     np.random.seed(cfg.seed)
     
+    profiler = Profiler()
+    profiler.start()
+
     msed = []
     for ii in range(NMC):
         t0 = time.time()
         cfg.seed += 1  # change seed for each run
         # Launch the simulation
         msed.append(Run(cfg).launch())
-        print(f"Monte Carlo run {ii + 1}/{NMC} completed in {time.time() - t0:.2f} s.")
-    print("All Monte Carlo runs completed.")
+        print(f"Monte Carlo run {ii + 1}/{NMC} completed in {time.time() - t0:.2f} s.", end='\r')
+    print("\nAll Monte Carlo runs completed.")
+
+    profiler.stop()
+    print(profiler.output_text(unicode=True, color=True))
 
     post_processing(msed, cfg)
     pass
@@ -57,14 +64,23 @@ def post_processing(msed: list[dict], cfg: Parameters):
     ]
 
     # Plot results
-    fig, axes = plt.subplots(1, 1)
+    fig, axes = plt.subplots(1, 2, gridspec_kw={
+        'width_ratios': [len(msed[0].keys()) - 1, 1]
+    })
     fig.set_size_inches(8.5, 3.5)
     violinWidth = 0.8
     for ii, BFtype in enumerate(msed[0].keys()):
+        # Data
+        data = np.mean(np.array([msed[jj][BFtype] for jj in range(NMC)]), axis=1)
+        if ii == len(msed[0].keys()) - 1:
+            idxAx = 1
+            posViolin = [0]
+        else:
+            idxAx = 0
+            posViolin = [ii]
         col = palette[ii]
         # Violin plot over MC runs, averaged over nodes
-        data = np.mean(np.array([msed[jj][BFtype] for jj in range(NMC)]), axis=1)
-        violinparts = axes.violinplot(data, positions=[ii], widths=violinWidth, showmeans=True, showextrema=False)
+        violinparts = axes[idxAx].violinplot(data, positions=posViolin, widths=violinWidth, showmeans=True, showextrema=False)
         for pc in violinparts['bodies']:
             pc.set_facecolor(col)
             pc.set_edgecolor(palette[-1])
@@ -75,20 +91,24 @@ def post_processing(msed: list[dict], cfg: Parameters):
             violinparts[partname].set_edgecolor('black')  # Change edges to black
             violinparts[partname].set_linewidth(2)
         # Plot scatter of individual runs, with random jitter
-        x = np.random.normal(ii, 0.1, len(data))
-        axes.scatter(x, data, marker='.', color=col)
-    axes.set_xticks(np.arange(len(msed[0].keys())))
-    axes.set_xticklabels(list(msed[0].keys()))
-    axes.set_xlim(-violinWidth/2, len(msed[0].keys()) - 1 + violinWidth/2)
-    # Add markers on y-axis to show mean values
-    for ii, BFtype in enumerate(msed[0].keys()):
-        data = np.mean(np.array([msed[jj][BFtype] for jj in range(NMC)]), axis=1)
-        axes.text(ii, np.amax(data), f"Mean:\n{np.mean(data):.2e}", ha='center', va='bottom')
-    # Adjust y-axis upper limit if text is too close to the top
-    if np.amax(data) > 0.9 * axes.get_ylim()[1]:
-        axes.set_ylim(top=1.2 * np.amax(data))
-    axes.set_title(f'Distribution of MSE_d over {NMC} MC runs (averages over {cfg.K} nodes)')
-    axes.grid()
+        xScatter = np.random.normal(posViolin[0], 0.1, len(data))
+        axes[idxAx].scatter(xScatter, data, marker='.', color=col)
+        # Add markers on y-axis to show mean values
+        axes[idxAx].text(posViolin[0], np.amax(data), f"Mean:\n{np.mean(data):.2e}", ha='center', va='bottom')
+        # Adjust y-axis upper limit if text is too close to the top
+        if np.amax(data) > 0.9 * axes[idxAx].get_ylim()[1]:
+            axes[idxAx].set_ylim(top=1.2 * np.amax(data))
+    axes[0].set_xticks(np.arange(len(msed[0].keys()) - 1))
+    axes[0].set_xticklabels(list(msed[0].keys())[:-1])
+    axes[0].set_xlim(-violinWidth/2, len(msed[0].keys()) - 2 + violinWidth/2)
+    axes[1].set_xticks([0])
+    axes[1].set_xticklabels([list(msed[0].keys())[-1]])
+    # Set axes[1] y-axis ticks and labels on the right
+    axes[1].yaxis.tick_right()
+    axes[1].yaxis.set_label_position('right')
+    for ax in axes:
+        ax.grid(axis='y')
+    fig.suptitle(f'Distribution of MSE_d over {NMC} MC runs (averages over {cfg.K} nodes)')
     fig.tight_layout()
     plt.show()
 
--- tools/algos.py
+++ tools/algos.py
@@ -5,9 +5,7 @@
 
 import copy
 import numpy as np
-import scipy.linalg as sla
 from .base import Parameters
-import matplotlib.pyplot as plt
 from dataclasses import dataclass
 
 @dataclass
@@ -66,26 +64,36 @@ class Run:
 
                 # --- LCMV beamformer ---
                 Qkq = QkqMat[k, q]  # number of common sources between k and q
-                zq = y[q][:Qkq, :]  # signal transmitted by q to k (Qkq x N)
-                # Compute the spatial covariance matrix (SCM) (batch)
-                Ryz = y[k] @ zq.T / c.N
-                # Compute the unwanted signal SCM (oracle)
-                Ru = (y[k] @ y[k].T - y[q] @ y[q].T) / c.N
+                # Compute the spatial covariance matrix (SCM)
+                if c.scmEst == 'theoretical':
+                    pass
+                elif c.scmEst == 'batch':
+                    zq = y[q][:Qkq, :]  # signal transmitted by q to k (Qkq x N)
+                    Ryz = y[k] @ zq.T / c.N
+                # Compute the unwanted signal SCM (oracle) <-- IMPORTANT ASSUMPTION for LCMV
+                if c.scmEst == 'theoretical':
+                    pass
+                elif c.scmEst == 'batch':
+                    Ru = (y[k] @ y[k].T - y[q] @ y[q].T) / c.N
                 Gam = np.linalg.inv(Ru)
                 # LCMV beamformer
                 Pkq['LCMV'] = Gam @ Ryz @ np.linalg.inv(Ryz.T @ Gam @ Ryz)  # (Mk x Qkq)
                 
                 # --- dMWF basic definition ---
-                Rykyk = y[k] @ y[k].T / c.N
-                # Figure out which latent sources are common between k and q
-                idxCommon_kq_d = np.where(oMatd[:, k] & oMatd[:, q])[0]
-                idxCommon_kq_n = np.where(oMatn[:, k] & oMatn[:, q])[0]
-                # if len(np.concatenate((idxCommon_kq_d, idxCommon_kq_n))) == 0:
-                #     print(f"No common sources between nodes {k} and {q}.")
-                skq = Amat[k][:, idxCommon_kq_d] @ latd[idxCommon_kq_d, :]
-                nkq = Bmat[k][:, idxCommon_kq_n] @ latn[idxCommon_kq_n, :]
-                gkq = skq + nkq + sn[k]
-                Rgkq = gkq @ gkq.T / c.N
+                if c.scmEst == 'theoretical':
+                    pass
+                elif c.scmEst == 'batch':
+                    Rykyk = y[k] @ y[k].T / c.N
+                # Figure out which latent sources are common between k and q  # <-- IMPORTANT ASSUMPTION for dMWF
+                if c.scmEst == 'theoretical':
+                    pass
+                elif c.scmEst == 'batch':
+                    idxCommon_kq_d = np.where(oMatd[:, k] & oMatd[:, q])[0]
+                    idxCommon_kq_n = np.where(oMatn[:, k] & oMatn[:, q])[0]
+                    skq = Amat[k][:, idxCommon_kq_d] @ latd[idxCommon_kq_d, :]
+                    nkq = Bmat[k][:, idxCommon_kq_n] @ latn[idxCommon_kq_n, :]
+                    gkq = skq + nkq + sn[k]
+                    Rgkq = gkq @ gkq.T / c.N
                 # Compute the MWF
                 Ekloc = np.zeros((c.Mk, Qkq))
                 Ekloc[:Qkq, :] = np.eye(Qkq)
@@ -113,8 +121,11 @@ class Run:
             for BFtype in zkq.keys():
                 ty = build_tilde(y[k], zkq[BFtype]['y'], k)
                 ts = build_tilde(s[k], zkq[BFtype]['s'], k)
-                Rty = ty @ ty.T / c.N
-                Rts = ts @ ts.T / c.N
+                if c.scmEst == 'theoretical':
+                    pass
+                elif c.scmEst == 'batch':
+                    Rty = ty @ ty.T / c.N
+                    Rts = ts @ ts.T / c.N
                 # Selection vector
                 tE = np.zeros((Rty.shape[0], c.D))
                 tE[:c.D, :] = np.eye(c.D)
@@ -125,8 +136,11 @@ class Run:
         # Compute centralized and local MWFs
         yc = np.concatenate(y, axis=0)
         sc = np.concatenate(s, axis=0)
-        Ryy = yc @ yc.T / c.N
-        Rss = sc @ sc.T / c.N
+        if c.scmEst == 'theoretical':
+            pass
+        elif c.scmEst == 'batch':
+            Ryy = yc @ yc.T / c.N
+            Rss = sc @ sc.T / c.N
         dhatk['Centralized'] = [None for _ in range(c.K)]
         dhatk['Local'] = [None for _ in range(c.K)]
         for k in range(c.K):
@@ -135,12 +149,18 @@ class Run:
             Wk = np.linalg.inv(Ryy) @ Rss @ Ek
             dhatk['Centralized'][k] = Wk.T @ yc
             # Local MWF
-            Rykyk = y[k] @ y[k].T / c.N
-            Rsksk = s[k] @ s[k].T / c.N
+            if c.scmEst == 'theoretical':
+                pass
+            elif c.scmEst == 'batch':
+                Rykyk = y[k] @ y[k].T / c.N
+                Rsksk = s[k] @ s[k].T / c.N
             Ekk = np.zeros((Rykyk.shape[0], c.D))
             Ekk[:c.D, :] = np.eye(c.D)
             Wkloc = np.linalg.inv(Rykyk) @ Rsksk @ Ekk
             dhatk['Local'][k] = Wkloc.T @ y[k]
+
+        # Compute unprocessed MSEd
+        dhatk['Unprocessed'] = [y[k][:c.D, :] for k in range(c.K)]  # unprocessed signal
 
         # Compute MSE_d
         msed = dict([
@@ -149,9 +169,6 @@ class Run:
                 for k in range(c.K)
             ]) for BFtype, dh in dhatk.items()
         ])
-        # Print nice results
-        # for BFtype, mse in msed.items():
-        #     print(f"MSE_d ({BFtype}): {mse}")
 
         return msed
         
